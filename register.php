019a36db53e257368cec6507a68ac5f31e97512f
Stilinski <realesdarren@gmail.com>
2024-09-18 12:05:08 +0800

    added sixteen changes

--- register.php
+++ register.php
@@ -1,3 +1,7 @@
+<?php
+include "php/insertUser.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +17,7 @@
         <div class="regFormCon">
         <h2 class="form-title">Registration</h2>
         <!-- action="<?//php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" -->
-            <form class="regForm" id="registrationForm" action="php/insertUser.php" method="post" enctype="multipart/form-data" autocomplete="OFF">
+            <form class="regForm" id="registrationForm"  method="post" enctype="multipart/form-data" autocomplete="OFF">
                 <div class="informations">
                     <div class="personal-info">
                         <div class="p-info">
